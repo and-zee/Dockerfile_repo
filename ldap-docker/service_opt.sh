873d00555f29b767f5302741c4eaf6ce687ae7d1
Andre Zee <social@andrezee.com>
2022-05-14 14:44:40 +0700

    add update file

---
+++ ldap-docker/service_opt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "=========================${RED}SERVER REOOTED${NC}=========================" >> /home/zee/service.log
+echo -e "==================$(date)==================" >> /home/zee/service.log
